276be9f77c2aef7cfebf9d75d34a1033d7caa4c4
mmunos <marc.munos@mce.gouv.qc.ca>
2023-08-02 14:56:02 -0400

    Modification du gulpfile.js de la génération de sprite pour copier les nouveaux sprites dans le répertoire img de /dist et /public

--- src/sprites/gulpfile.js
+++ src/sprites/gulpfile.js
@@ -1,33 +1,41 @@
-﻿(function () {
-    "use strict";
+﻿const
+    gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    svgSprite = require('gulp-svg-sprite'),
 
-    var gulp = require('gulp'),
-        svgSprite = require('gulp-svg-sprite'),
-
-        // Configuration
-        config = {
-            mode: {                
-                view: {
-                    bust: false,
-                    layout: "vertical"
-                }
-                
-            },
-            shape: {
-                spacing: { // Spacing related options
-                    padding: 1, // Padding around all shapes
-                    box: 'content' // Padding strategy (similar to CSS `box-sizing`)
-                }
-            },
-            svg: {
-                precision: 0,
-                dimensionAttributes: false
+    // Configuration
+    config = {
+        mode: {
+            view: {
+                bust: false,
+                layout: "vertical"
             }
-        };
 
-    gulp.task('generateSprite', function () {
-        return gulp.src('./*.svg', { cwd: './svg' })
-            .pipe(svgSprite(config))
-            .pipe(gulp.dest('dist'));
-    });
-})();
+        },
+        shape: {
+            spacing: { // Spacing related options
+                padding: 1, // Padding around all shapes
+                box: 'content' // Padding strategy (similar to CSS `box-sizing`)
+            }
+        },
+        svg: {
+            precision: 0,
+            dimensionAttributes: false
+        }
+    };
+
+function doGenerateSprite() {
+    return gulp.src('./*.svg', { cwd: './svg' })
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('dist'));;
+}
+
+function copy() {
+    return gulp.src('./sprite.view.svg', { cwd: './dist/view/svg' })
+            .pipe(rename('qc-sprite.svg'))
+            .pipe(gulp.dest('img', {cwd: ['../../dist']}))
+            .pipe(gulp.dest('img', {cwd: ['../../public']}))
+        ;
+}
+
+exports.generateSprite =  gulp.series(doGenerateSprite,copy)
