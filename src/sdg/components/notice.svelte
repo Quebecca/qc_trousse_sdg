7a2037f814e93ca7f43934ac768e07d665bff7ed
mmunos <marc.munos@mce.gouv.qc.ca>
2024-12-06 16:18:12 -0500

    Avis - intégration du composant icône. Ajout de l'attribut icon pour le choix de l'image. Ajout du type neutre (neutral)

--- src/sdg/components/notice.svelte
+++ src/sdg/components/notice.svelte
@@ -3,21 +3,26 @@
 <script>
   import { Utils } from "./utils"
   import { onMount } from "svelte";
+  import Icon from "./Icon.svelte";
 
   const
-      defaultHeader = 'h2'
-    , defaultType = 'information'
-    , types =
-        [ 'information'
-        , 'warning'
-        , 'success'
-        , 'error'
-        ]
+      isFr = Utils.getPageLanguage() == 'fr'
+    , defaultHeader = 'h2'
+    , defaultType = 'neutral'
+          , typesDescriptions = {
+            'information': isFr ? "Avis général" : "General notice"
+            , 'warning': isFr ? "Avis d’avertissement" : "Warning notice"
+            , 'success': isFr ? "Avis de réussite" : "Success notice"
+            , 'error': isFr ? "Avis d’erreur" : "Error notice"
+          }
+    , types = Object.keys(typesDescriptions)
   export let
           title = ""
           , type = defaultType
           , content = ""
           , header = defaultHeader
+          , icon
+          , iconLabel = typesDescriptions[type] ?? ''
 
   onMount(() => {
     header = header.match(/h[1-6]/)
@@ -36,8 +41,11 @@
 <div class="qc-component qc-notice qc-{type}"
      tabindex="0">
   <div class="icon-container">
-    <div aria-hidden="true"
-         class="qc-icon qc-{type}">
+    <div class="qc-icon">
+      <Icon type="{icon ?? (type == 'neutral' ? 'information' : type)}"
+            label={iconLabel}
+            size="nm"
+      />
     </div>
   </div>
   <div class="content-container">
@@ -53,4 +61,21 @@
     </div>
   </div>
 </div>
+<style lang="scss">
+  @each $type, $color in (
+          "information": "blue",
+          "warning": "yellow",
+          "neutral": "grey",
+          "error": "red",
+          "success": "green",
+  ) {
+    .qc-notice.qc-#{$type} .icon-container {
+      background-color: token-value(color,$color,pale);
+      :global([role=img]) {
+        background-color: token-value(color,$color,dark);
+      }
+    }
+  }
+</style>
+
 <link rel='stylesheet' href='{Utils.cssRelativePath}{Utils.cssFileName}'>
