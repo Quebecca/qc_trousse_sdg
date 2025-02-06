0c77cfc1f24b3b5d7734b2dbfd052374533f7329
mmunos <marc.munos@mce.gouv.qc.ca>
2025-02-06 14:14:00 -0500

    SearchInput - correction concernant l'attribut aria-label

--- src/sdg/components/SearchInput/SearchInput.svelte
+++ src/sdg/components/SearchInput/SearchInput.svelte
@@ -1,11 +1,16 @@
 <svelte:options customElement="{{
     tag: 'qc-search-input',
-    shadow: 'none'
+    shadow: 'none',
+    props: {
+        ariaLabel: {attribute:'aria-label'}
+    }
 }}" />
 <script>
     import IconButton from "../Button/IconButton.svelte";
     export let value, ariaLabel;
     let searchInput;
+
+
 </script>
 <div class="qc-search-input">
     <input  bind:this={searchInput}
@@ -13,6 +18,7 @@
             type="search"
             class="control"
             autocomplete="off"
+            {...(ariaLabel ? {"aria-label": ariaLabel} : {})}
             {...$$restProps}
     />
     {#if value}
