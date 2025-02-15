922515127c6149c1f314e6e3bd82ba60e4c12489
mmunos <marc.munos@mce.gouv.qc.ca>
2025-02-15 15:10:01 -0500

    Correction des liens dans le bandeau PIV + reste de la page (problème de focus)

--- src/doc/components/Switch.svelte
+++ src/doc/components/Switch.svelte
@@ -52,10 +52,9 @@
     cursor: pointer;
     margin:0;
     padding:0;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    inset: 0;
+    height: auto;
+    width: auto;
   }
 
   /* The slider */
