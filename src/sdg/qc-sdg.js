51f50a0ee3a496539044487471e8b718078e5f3d
mmunos <marc.munos@mce.gouv.qc.ca>
2025-04-16 14:43:33 -0400

    Séparation des différentes parties de qc-sdg.js (pour permettre des recompilations dans des projets ajoutant la trousse comme dépendance)

--- src/sdg/qc-sdg.js
+++ src/sdg/qc-sdg.js
@@ -1,15 +1,4 @@
-export * from './components/notice.svelte'
-export * from './components/PivHeader/pivHeader.svelte'
-export * from './components/pivFooter.svelte'
-export * from './components/alert.svelte'
-export * from './components/toTop.svelte'
-export * from './components/externalLink.svelte'
-export * from './components/SearchBar/searchBar.svelte'
-export * from './components/SearchInput/SearchInput.svelte'
-export * from './components/Icon.svelte'
+import "./_components"
+import "./_dark-theme"
 import './scss/qc-sdg.scss' ;
 
-const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (isDarkMode) {
-    document.documentElement.classList.add('qc-dark-theme')
-}
