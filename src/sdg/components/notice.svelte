1b6a2e39dec22f6c655caf18a871a3ee728b85b3
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-05-15 14:22:30 -0400

    Mise a jour de notice.svelte vers la syntaxe de Svelte 5

--- src/sdg/components/notice.svelte
+++ src/sdg/components/notice.svelte
@@ -30,25 +30,29 @@
   const usedHeader = header.match(/h[1-6]/) ? header : defaultHeader;
   const role = usedType === "success" ? "status" : (usedType === "error" ? "alert" : null);
 
-  let noticeElement;
-  if (role && noticeElement) {
-      const tempNodes = Array.from(noticeElement.childNodes);
-      noticeElement.innerHTML = "";
+  let noticeElement = $state(null);
+  $effect(() => {
+      if (role && noticeElement) {
+          const tempNodes = Array.from(noticeElement.childNodes);
+          noticeElement.innerHTML = "";
 
-      // Réinsère le contenu pour qu'il soit détecté par le lecteur d'écran.
-      tempNodes.forEach(node => noticeElement.appendChild(node));
-  }
+          // Réinsère le contenu pour qu'il soit détecté par le lecteur d'écran.
+          tempNodes.forEach(node => noticeElement.appendChild(node));
+      }
+  });
 
-  const shouldUseIcon = type === "advice" || type === "note";
+  const shouldUseIcon = usedType === "advice" || usedType === "note";
 
     // Si le type est "advice" ou "note", on force "neutral" (le gris), sinon on garde le type normal
-    const computedType = shouldUseIcon ? "neutral" : type;
-    const iconType = shouldUseIcon ? (icon ?? "note") : type;
+    const computedType = shouldUseIcon ? "neutral" : usedType;
+    const iconType = shouldUseIcon ? (icon ?? "note") : usedType;
     const iconLabel = typesDescriptions[type] ?? typesDescriptions['information'];
 </script>
 
 <!-- TODO Confirmer tabindex. On pense que c'est important de recevoir le focus (notamment dans les formulaires) en fait pour nous c'est très important, sinon en mode formulaire au lecteur écran (nav avec TAB, ce n'est jamais lu). -->
-
+<!--Le warning `Svelte: noninteractive element cannot have nonnegative tabIndex value`
+associe a tabindex="0" doit etre ignore pour qu'on puisse focus sur le composant sans
+le denaturaliser.-->
 <div class="qc-component qc-notice qc-{computedType}" tabindex="0">
   <div class="icon-container">
     <div class="qc-icon">
