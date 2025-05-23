e0094a9aa2e67967744f60c38c6b85e5e8981fe4
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-05-23 15:50:15 -0400

    Creation du wrapper pour RadioButton.svelte

--- src/sdg/components/FieldInputs/RadioButton/RadioButton.svelte
+++ src/sdg/components/FieldInputs/RadioButton/RadioButton.svelte
@@ -1,15 +1,3 @@
-<svelte:options customElement="{{
-    tag: 'qc-radio-button',
-    shadow: 'none',
-    props: {
-        name: {attribute:'name'},
-        value: {attribute:'value'},
-        size: {attribute:'size'},
-        checked: {attribute: 'checked'},
-        disabled: {attribute:'disabled'}
-    }
-}}" />
-
 <script>
     import {Utils} from "../../utils";
 
@@ -22,8 +10,6 @@
     } = $props();
 
     const usedSize = size === "sm" ? "sm" : "md";
-
-    // const displayedValue = value.startsWith("_") ? value.substring(1) : value;
 </script>
 
 <div>
