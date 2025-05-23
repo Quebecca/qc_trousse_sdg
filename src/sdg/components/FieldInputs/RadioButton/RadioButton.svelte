f36c4fd3ba7f93cfaf7c14d183ce78dcdd758487
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-05-23 08:57:31 -0400

    Ajout de qc-fieldset dans les composants exportes

---
+++ src/sdg/components/FieldInputs/RadioButton/RadioButton.svelte
@@ -0,0 +1,22 @@
+<script>
+    let {
+        name,
+        value,
+        size,
+    } = $props();
+
+    const usedSize = size === "sm" ? "sm" : "md";
+
+    const displayedValue = value.startsWith("_") ? value.substring(1) : value;
+</script>
+
+<div>
+    <input
+        type="radio"
+        id={value}
+        name={name}
+        value={value}
+        disabled={value.startsWith("_")}
+    />
+    <label for={value}>{displayedValue}</label>
+</div>
