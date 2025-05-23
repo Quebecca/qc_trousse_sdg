5a2fc64c32425c9cb4af384aaf41021299a547d3
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-05-23 11:48:13 -0400

    Creation de bouton-radio-v2

--- src/sdg/components/FieldInputs/Fieldset.svelte
+++ src/sdg/components/FieldInputs/Fieldset.svelte
@@ -1,27 +1,9 @@
-<svelte:options customElement="{{
-    tag: 'qc-fieldset',
-    shadow: 'none',
-    props: {
-        fieldInputType: {attribute:'field-input-type'},
-        fieldLegendName: {attribute:'field-legend-name'},
-        fieldValues: {attribute:'field-values'},
-        fieldName: {attribute:'field-name'},
-        fieldDescribedBy: {attribute: 'field-described-by'}
-    }
-}}" />
-
 <script>
-    import RadioButton from "./RadioButton/RadioButton.svelte";
-
     let {
-        fieldInputType = "radio",
         fieldLegendName = "",
-        fieldValues = $bindable([]),
-        fieldName = "",
         fieldDescribedBy = "",
+        options
     } = $props();
-
-    let valuesArray = $derived(JSON.parse(fieldValues.replace(/'/g, '"')));
 </script>
 
 <fieldset aria-describedby={fieldDescribedBy}>
@@ -30,12 +12,7 @@
     {/if}
 
 
-    {#if fieldInputType === "radio"}
-        {#each valuesArray as v}
-            <RadioButton
-                    name={fieldName}
-                    value={v}
-            />
-        {/each}
-    {/if}
+    <div>
+        {@render options?.()}
+    </div>
 </fieldset>
---
+++ src/sdg/components/FieldInputs/FieldsetWC.svelte
@@ -0,0 +1,26 @@
+<svelte:options customElement="{{
+    tag: 'qc-fieldset',
+    props: {
+        fieldLegendName: {attribute:'field-legend-name'},
+        fieldDescribedBy: {attribute: 'field-described-by'}
+    }
+}}" />
+
+<script>
+    import Fieldset from "./Fieldset.svelte";
+
+    let { fieldLegendName, fieldDescribedBy } = $props();
+</script>
+
+{#snippet options()}
+    {@const contentHtml = `<slot />`}
+    {@html contentHtml}
+{/snippet}
+
+<div>
+    <Fieldset
+        {fieldLegendName}
+        {fieldDescribedBy}
+        {options}
+    />
+</div>
