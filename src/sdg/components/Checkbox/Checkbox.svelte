5990476b29e8f64f9382c0fc13e0661100fe5b49
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-05-28 13:41:53 -0400

    Ajout du composant Case à cocher fonctionnel (sans style pour l'instant)

---
+++ src/sdg/components/Checkbox/Checkbox.svelte
@@ -0,0 +1,30 @@
+<script>
+    const {
+        checkboxName,
+        checkboxValue,
+        checkboxSize = "md",
+        checkboxChecked = false,
+        checkboxDisabled = false,
+        checkboxLabel = ""
+    } = $props();
+
+    const id = `${checkboxName}_${checkboxValue}`;
+</script>
+
+<div class={checkboxSize === "sm" ? "qc-form-check-sm" : "qc-form-check"}>
+    <input
+        class="qc-form-check-input"
+        type="checkbox"
+        id={id}
+        name={checkboxName}
+        value={checkboxValue}
+        checked={checkboxChecked}
+        disabled={checkboxDisabled}
+        />
+    <label for={id}>{checkboxLabel || checkboxValue}</label>
+</div>
+
+
+<style>
+
+</style>
---
+++ src/sdg/components/Checkbox/FieldsetCheckboxWC.svelte
@@ -0,0 +1,24 @@
+
+<svelte:options customElement={{
+    tag: 'qc-checkbox-fieldset',
+    shadow: 'none',
+    props: {
+        checkboxLegend: { attribute: 'checkbox-legend' },
+        checkboxSize: { attribute: 'checkbox-size' },
+        checkboxName: { attribute: 'checkbox-name' }
+    }
+}} />
+
+<script>
+    import FieldsetCheckbox from "./FieldsetCheckbox.svelte";
+    const { checkboxLegend, checkboxSize, checkboxName } = $props();
+    const checkboxes = document.querySelectorAll("qc-checkbox");
+</script>
+
+
+<FieldsetCheckbox
+    {checkboxLegend}
+    {checkboxSize}
+    {checkboxName}
+    {checkboxes}
+/>
