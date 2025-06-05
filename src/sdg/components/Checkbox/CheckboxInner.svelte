b46d507febb4b2590a49de362abf56a6bdba8aee
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-06-05 17:08:19 -0400

    Ajout des composants "Inner" et "Outer"

---
+++ src/sdg/components/Checkbox/CheckboxInner.svelte
@@ -0,0 +1,18 @@
+<script>
+    let { value, label, name } = $props();
+    let id = $derived(name + "_" + value);
+</script>
+
+<div>
+    <input
+        type="checkbox"
+        bind:value
+        {name}
+        {id}
+    />
+    <label for={id}>{label}</label>
+</div>
+
+<style>
+
+</style>
---
+++ src/sdg/components/Checkbox/CheckboxOuter.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { setContext, onMount } from 'svelte';
+    let { inners, legend, name } = $props();
+    let pseudo;
+    setContext('name', {name});
+
+    onMount(() => {
+        inners.forEach(
+            inner => pseudo.appendChild(inner)
+        )
+    })
+</script>
+
+<fieldset>
+    <legend>{legend}</legend>
+    <div id="pseudo-slot" bind:this={pseudo}></div>
+</fieldset>
+
+<style>
+
+</style>
