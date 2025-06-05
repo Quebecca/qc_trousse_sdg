a3f66c9669bebbf10ea4e621b055e113c5302e85
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-06-05 17:08:19 -0400

    Ajout des composants "Inner" et "Outer"

---
+++ src/sdg/components/Checkbox/CheckboxInnerWC.svelte
@@ -0,0 +1,30 @@
+<svelte:options customElement={{
+    tag: 'qc-checkbox-inner',
+    shadow: 'none',
+    props: {
+        value: { attribute: 'value', type: 'String' },
+        label: { attribute: 'label', type: 'String' },
+        name: { attribute: 'name', type: 'String' }
+    },
+    extend: (customElementConstructor) => {
+        return class extends customElementConstructor {
+            static inner;
+            static outer;
+
+            constructor() {
+                super();
+                this.inner = this;
+                this.outer = this.closest('qc-checkbox-outer');
+            }
+        };
+    }
+}} />
+
+<script>
+
+    import CheckboxInner from "./CheckboxInner.svelte";
+    import {onMount} from "svelte";
+    let {inner, outer, value, label, name} = $props();
+</script>
+
+<CheckboxInner {value} {label} name={outer?.name ?? name}></CheckboxInner>
