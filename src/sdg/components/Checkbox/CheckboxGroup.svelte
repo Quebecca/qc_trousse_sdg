4ccc66173a32c1cc20c68fe7d92a42013eb17520
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-06-10 10:48:56 -0400

    Espacement entre les cases corrigé

--- src/sdg/components/Checkbox/CheckboxGroup.svelte
+++ src/sdg/components/Checkbox/CheckboxGroup.svelte
@@ -20,7 +20,7 @@
 
 <fieldset class ="checkbox-group-{size}">
     <legend>{legend}</legend>
-    <div id="pseudo-slot" bind:this={pseudo}></div>
+    <div id="pseudo-slot" class ="checkbox-group-{size}" bind:this={pseudo}></div>
 </fieldset>
 
 <style>
