1fa364b361b0f912d9596406363be131a3f8c0ed
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-06-26 12:16:23 -0400

    Ajout de composant ToggleSwitch

---
+++ src/sdg/components/ToggleSwitch/ToggleSwitch.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {onMount} from "svelte";
+    import {Utils} from "../utils";
+
+    let {
+        name,
+        handleOnChange,
+        checked = $bindable(false),
+        ...rest
+    } = $props();
+
+    let restProps = $state({});
+    onMount(() => {
+        const [inputProps] = Utils.computeFieldsAttributes(["switch"], {}, rest);
+
+        restProps = {...inputProps};
+    });
+</script>
+
+<label class="qc-switch">
+    <input
+        type="checkbox"
+        role="switch"
+        {name}
+        bind:checked={checked}
+        aria-checked={checked}
+        onchange={handleOnChange}
+        {...restProps}
+    />
+    <span class="qc-switch-slider"></span>
+</label>
---
+++ src/sdg/components/ToggleSwitch/ToggleSwitchWC.svelte
@@ -0,0 +1,27 @@
+<svelte:options customElement = {{
+    tag: 'qc-toggle-switch',
+    shadow: 'none',
+    props: {
+        name: {attribute: 'name', type: 'String'},
+        handleOnChange: {attribute: 'handle-on-change', type: 'String'},
+        checked: {attribute: 'checked', type: 'Boolean'},
+    },
+}} />
+
+<script>
+    import ToggleSwitch from "./ToggleSwitch.svelte";
+
+    let {
+        name,
+        handleOnChange,
+        checked = $bindable(false),
+        ...rest
+    } = $props();
+</script>
+
+<ToggleSwitch
+    {name}
+    {handleOnChange}
+    {checked}
+    {...rest}
+/>
