ea4367905238559302bc8f7aaff8ee3b98cb6062
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-07-07 15:25:37 -0400

    Ajout du composant TextField avec tous les changements d'architecture nécessaires fait dans champ-texte initialement

--- src/sdg/components/FormError/FormError.svelte
+++ src/sdg/components/FormError/FormError.svelte
@@ -1,21 +1,24 @@
 <script>
 import Icon from "../Icon/Icon.svelte";
 import { tick } from "svelte";
-let {invalid , invalidText} = $props();
+
+let {invalid , invalidText, id} = $props();
+
 </script>
 {#if invalid}
-<div class="qc-form-error"
-     role="alert">
-    {#await tick()}
-    <!-- svelte-ignore block_empty -->
-    {:then}
-    <Icon
-                type="warning"
-                color="red-regular"
-                width="var(--error-icon-width)"
-                height="var(--error-icon-height)"
-        />
-        <span>{@html invalidText}</span>
-    {/await}
-</div>
+    <div class="qc-form-error"
+         role="alert"
+         {id}>
+        {#await tick()}
+            <!-- svelte-ignore block_empty -->
+        {:then}
+            <Icon
+                    type="warning"
+                    color="red-regular"
+                    width="var(--error-icon-width)"
+                    height="var(--error-icon-height)"
+            />
+            <span>{@html invalidText}</span>
+        {/await}
+    </div>
 {/if}
