f1a9173d69c48b18f6bfb086f3d32c9f719bea69
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-07-08 15:28:33 -0400

    Gestion de tab pour quitter la liste deroulante

--- src/sdg/components/DropdownList/DropdownListSingle.svelte
+++ src/sdg/components/DropdownList/DropdownListSingle.svelte
@@ -1,5 +1,5 @@
 <script>
-    let { items, passValue = () => {} } = $props();
+    let { items, passValue = () => {}, handleExitTab = () => {} } = $props();
 
     let predecessor = $state();
     let selectedValue = $state();
@@ -17,21 +17,25 @@
         passValue(value);
     }
 
-    function handleKeyDown (event, value) {
+    function handleKeyDown (event, value, index) {
         if (event.key === 'Enter' || event.key === ' ') {
             handleEvent(event.target, value);
+        }
+
+        if (event.key === "Tab" && index === items.length - 1) {
+            handleExitTab();
         }
     }
 </script>
 
-{#each items as item}
+{#each items as item, index}
     <div
         class="qc-dropdown-list-single"
         tabindex="0"
         role="option"
         aria-selected="{selectedValue === item.value ? 'true' : 'false'}"
         onclick={(event) => handleEvent(event.target, item.value)}
-        onkeydown={(event) => handleKeyDown(event, item.value)}
+        onkeydown={(event) => handleKeyDown(event, item.value, index)}
     >
         {item.label}
     </div>
