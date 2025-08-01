1918c6cda9415b85f79fe5bb6022607d030beb6f
mmunos <marc.munos@mce.gouv.qc.ca>
2025-08-01 14:51:03 -0400

    Test - ajout de playwright + un test pour les boutons Radios

---
+++ tests/radio-button.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+const path = require('path');
+
+test('test', async ({ page }) => {
+    const htmlFilePath = path.resolve(__dirname, '../public/radioButton.dev.html');
+      // Navigue vers le fichier en utilisant le protocole file://
+      await page.goto(`file://${htmlFilePath}`);
+      await page.locator('#exemple-radio-md').getByText('Option 2').click();
+        await page.getByRole('button', { name: 'Soumettre' }).click();
+        await expect(page.locator('#radio-group')).toMatchAriaSnapshot(`
+          - group "Quel est votre niveau de scolarité?":
+            - radio "Secondaire"
+            - radio "Professionnel"
+            - radio "Collégial"
+            - radio "Universitaire"
+            - alert:
+              - img
+              - text: Veuillez sélectionner un niveau de scolarité.
+          `);
+        await page.getByText('Universitaire', { exact: true }).click();
+        await expect(page.locator('#radio-group')).toMatchAriaSnapshot(`
+          - group "Quel est votre niveau de scolarité?":
+            - radio "Secondaire"
+            - radio "Professionnel"
+            - radio "Collégial"
+            - radio "Universitaire" [checked]
+          `);
+        page.once('dialog', dialog => {
+          console.log(`Dialog message: ${dialog.message()}`);
+          dialog.dismiss().catch(() => {});
+        });
+        await page.getByRole('button', { name: 'Soumettre' }).click();
+
+});
