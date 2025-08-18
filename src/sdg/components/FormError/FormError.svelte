58b21d352ccb14aa184641c3a5e7e7f8f41469a7
mmunos <marc.munos@mce.gouv.qc.ca>
2025-08-18 15:38:48 -0400

    FormError - modification du message par defaut

--- src/sdg/components/FormError/FormError.svelte
+++ src/sdg/components/FormError/FormError.svelte
@@ -3,16 +3,28 @@ import Icon from "../Icon/Icon.svelte";
 import {onMount, tick} from "svelte";
 import {Utils} from "../utils";
 
+const lang = Utils.getPageLanguage();
+
 let {invalid ,
+    label,
     invalidText,
     id = $bindable(),
     extraClasses = [],
     rootElement = $bindable(),
 } = $props();
 
+let cleanLabel = $derived(label.replace(/:\s*$/, '')),
+    defaultInvalidText = $derived(
+        lang === 'fr'
+            ? `Le champ ${cleanLabel} est requis.`
+            : `${cleanLabel} is required.`
+    )
+;
+
 onMount(() => {
     id = Utils.generateId('qc-form-error')
 })
+
 </script>
 {#if invalid}
 <div {id}
@@ -28,7 +40,7 @@ onMount(() => {
                 width="var(--error-icon-width)"
                 height="var(--error-icon-height)"
         />
-        <span>{@html invalidText}</span>
+        <span>{@html invalidText ? invalidText : defaultInvalidText}</span>
     {/await}
 </div>
 {/if}
