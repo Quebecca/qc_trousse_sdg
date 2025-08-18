d933ec490c1b5792ae64be150cccaaa4b56b4b00
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-08-18 16:38:14 -0400

    Correction de l'export de DropdownListEmbeddedTest

---
+++ src/sdg/components/DropdownList/Test/DropdownListTest.svelte
@@ -0,0 +1,56 @@
+<svelte:options customElement={{
+  tag: 'qc-select-embedded-test'
+}} />
+
+<script>
+    import DropdownList from "../DropdownList.svelte";
+    import {Utils} from "../../utils";
+    import {onMount} from "svelte";
+
+    let genericOptionsDisplay = $state([]);
+    onMount(() => {
+        let genericOptions = [];
+        for (let i = 0; i < 16; i++) {
+            console.log(i);
+            genericOptions.push({
+                value: i,
+                label: `Option ${i}`,
+                disabled: false,
+                checked: false,
+            });
+        }
+        genericOptionsDisplay = genericOptions;
+    });
+
+    $inspect(genericOptionsDisplay);
+</script>
+<div style="background-color: white; width: 200px; height: 40px;"></div>
+
+    <DropdownList
+            id="dropdown-list-single-choice"
+            label="Choix unique:"
+            items={JSON.parse(JSON.stringify(genericOptionsDisplay))}
+    />
+
+    <DropdownList
+            id="dropdown-list-single-choice"
+            label="Choix unique avec recherche"
+            enableSearch={true}
+            items={JSON.parse(JSON.stringify(genericOptionsDisplay))}
+    />
+
+    <DropdownList
+            id="dropdown-list-multiple-choices"
+            label="Choix multiples"
+            multiple
+            items={JSON.parse(JSON.stringify(genericOptionsDisplay))}
+    />
+
+    <DropdownList
+            id="dropdown-list-invalid"
+            label="État invalide"
+            disabled
+            items={JSON.parse(JSON.stringify(genericOptionsDisplay))}
+    />
+
+<link rel='stylesheet' href='{Utils.cssPath}'>
