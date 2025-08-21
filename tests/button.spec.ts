1c339a3af1bc790a02843525373927f0bcb81fd4
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-08-21 10:00:10 -0400

    Ajout des tests des styles hover, focus et active dans buttonTest.html

--- tests/button.spec.ts
+++ tests/button.spec.ts
@@ -14,3 +14,42 @@ test('button svelte', async ({ page }) => {
     await page.goto(`file://${htmlFilePath}`);
     await expect(page).toHaveScreenshot('button.png', { fullPage: true });
 });
+
+test('button-primary, hover style is applied on hover', async ({ page }) => {
+    const htmlFilePath = path.resolve(__dirname, '../public/button.test.html');
+    await page.goto(`file://${htmlFilePath}`);
+    const button = page.locator('.qc-button.qc-primary').first();
+
+    await button.hover();
+    await page.waitForTimeout(300);
+    const bgColor = await button.evaluate(el => getComputedStyle(el).backgroundColor);
+    const borderColor = await button.evaluate(el => getComputedStyle(el).borderColor);
+    expect(bgColor).toBe('rgb(20, 114, 191)')
+    expect(borderColor).toBe('rgb(20, 114, 191)')
+
+});
+
+test('button-primary, focus style is applied on focus', async ({ page }) => {
+    const htmlFilePath = path.resolve(__dirname, '../public/button.test.html');
+    await page.goto(`file://${htmlFilePath}`);
+    const button = page.locator('.qc-button.qc-primary').first();
+
+    await button.focus();
+    await page.waitForTimeout(300);
+    const boxShadow = await button.evaluate(el => getComputedStyle(el).boxShadow);
+
+    expect(boxShadow).not.toBe('none');
+});
+
+test('button-primary, active style is applied on mousedown', async ({ page }) => {
+    const htmlFilePath = path.resolve(__dirname, '../public/button.test.html');
+    await page.goto(`file://${htmlFilePath}`);
+    const button = page.locator('.qc-button.qc-primary').first();
+
+    const before = await button.evaluate(el => getComputedStyle(el).backgroundColor);
+    await button.dispatchEvent('mousedown');
+    const during = await button.evaluate(el => getComputedStyle(el).backgroundColor);
+    await button.dispatchEvent('mouseup');
+
+    expect(during).not.toEqual(before);
+});
