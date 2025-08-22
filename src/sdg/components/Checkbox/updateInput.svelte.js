625d7fd82232e7d1c357f3808b5d4715bd6c495e
mmunos <marc.munos@mce.gouv.qc.ca>
2025-08-22 15:39:30 -0400

    ChoiceGroup - correction de la regression lié au disfonctionnement de l'attribut name

--- src/sdg/components/Checkbox/updateInput.svelte.js
+++ src/sdg/components/Checkbox/updateInput.svelte.js
@@ -1,5 +1,9 @@
-export function updateInput(input, required, invalid) {
+export function updateInput(input, required, invalid, name) {
     if (!input) return;
     input.setAttribute('aria-required', required ? 'true' : "false")
     input.setAttribute('aria-invalid', invalid ? 'true' : "false")
+    if (name && !input.hasAttribute('name')) {
+        input.setAttribute('name', name)
+    }
+
 }
