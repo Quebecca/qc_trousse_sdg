0cebca73a904c1170d5cd5b360457d522a1634fd
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-08-28 16:02:53 -0400

    Ajout des tests playwright pour `qc-icon`, Suppression des fichiers embeddedTest pour `qc-button`

--- src/sdg/qc-sdg-test.js
+++ src/sdg/qc-sdg-test.js
@@ -1,5 +1,5 @@
+export * from './bases/Icon/Test/IconEmbeddedTest.svelte';
 export * from './components/TextField/Test/TextFieldEmbededTest.svelte';
-export * from './components/Button/Test/ButtonEmbeddedTest.svelte';
 export * from "./components/DropdownList/Test/DropdownListEmbeddedTest.svelte";
 export * from "./components/ChoiceGroup/Test/ChoiceGroupeEmbededTest.svelte";
 export * from "./components/ToggleSwitch/Test/ToggleSwitchEmbeddedTest.svelte";
