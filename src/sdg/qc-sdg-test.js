5a3afdcff47d4853753ac7b214d1b782d4906dcd
Vincent Bois <Vincent.Bois@mce.gouv.qc.ca>
2025-08-29 15:21:46 -0400

    Ajout des tests playwright pour `qc-piv-header` (composant web). Tests du composant svelte (embedded) à compléter.

--- src/sdg/qc-sdg-test.js
+++ src/sdg/qc-sdg-test.js
@@ -3,3 +3,4 @@ export * from './components/TextField/Test/TextFieldEmbededTest.svelte';
 export * from "./components/DropdownList/Test/DropdownListEmbeddedTest.svelte";
 export * from "./components/ChoiceGroup/Test/ChoiceGroupeEmbededTest.svelte";
 export * from "./components/ToggleSwitch/Test/ToggleSwitchEmbeddedTest.svelte";
+export * from "./components/PivHeader/Test/pivHeaderEmbeddedTest.svelte";
