f0cfb0a6083bda5b1eea3d7fc7ed99f8f1a636c2
Gabriel-Bilodeau <gabriel.bilodeau@mce.gouv.qc.ca>
2025-11-24 16:35:50 -0500

    WIP test de lien visité (non-fonctionnel pour l'instant)

--- tests/external-link-baseline.spec.ts
+++ tests/external-link-baseline.spec.ts
@@ -1,15 +1,41 @@
-import {test} from "@playwright/test";
+import {expect, test} from "@playwright/test";
 import path = require('path');
 
 test.beforeEach(async ({ page }) => {
     const htmlFilePath = path.resolve(__dirname, '../public/externalLinkBaseline.test.html');
-    await page.goto(`file://${htmlFilePath}`);
+    await page.goto(`http://localhost:8080/public/externalLinkBaseline.test.html`);
 });
 
-// TODO trouver comment aller forcer le statut visité sur un lien externe
-// test.describe('Rendu visuel', () => {
-//     test(
-//     'should open in new tab', async ({ page }) => {
-//         await page.getByRole('link', { name: 'https://www.quebec.ca/' });
-//     });
-// });
+test.describe('Rendu visuel', () => {
+    test(
+    'Survol de lien', {
+            tag: ['@baseline', '@external-link']
+        }, async ({ page }) => {
+        await page.getByRole('link', { name: 'Lorem ipsum dolor sit amet,' }).first().hover();
+
+        await expect(page).toHaveScreenshot('externalLinkHover.png', {fullPage: true});
+    });
+
+    test('Lien visité', {
+        tag: ['@baseline', '@external-link']
+    }, async ({ page }) => {
+        const htmlFilePath = path.resolve(__dirname, '../public/externalLinkBaseline.test.html');
+
+        // Cliquer sur le lien
+        await page.getByRole('link', { name: 'https://www.quebec.ca/' }).click();
+
+        // Attendre que l'URL change
+        await page.waitForURL(`${htmlFilePath}#multi-links`);
+
+        // Remonter en haut de la page si nécessaire
+        await page.evaluate(() => window.scrollTo(0, 0));
+
+        await page.waitForTimeout(100);
+
+        await expect(page).toHaveScreenshot('externalLinkVisit.png', {fullPage: true});
+
+        await page.getByRole('link', { name: 'Lorem ipsum dolor sit amet,' }).first().focus();
+
+        await expect(page).toHaveScreenshot('externalLinkBluredVisited.png', {fullPage: true});
+    });
+});
