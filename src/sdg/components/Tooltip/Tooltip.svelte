11b5385bc29a51fcbbb3208b0f5daeac67d3fb5a
mmunos <marc.munos@mce.gouv.qc.ca>
2025-12-03 11:06:31 -0500

    wip

--- src/sdg/components/Tooltip/Tooltip.svelte
+++ src/sdg/components/Tooltip/Tooltip.svelte
@@ -9,8 +9,36 @@
     let src = Utils.imagesRelativePath + "info-tooltip.svg";
 </script>
 {text} {description}
-<IconButton
-        icon="info-tooltip"
+<span class="qc-tooltip">
+ {#if text}
+     <span class="qc-tooltip-text">{@html text}</span>
+ {/if}
+ {#if text}
+     <a role="button"
+        class="qc-tooltip-button"
+        href="#top"
+        onclick={e => e.preventDefault()}
+        ><Icon type="info-tooltip" size="sm" />
+     </a>
+ {/if}
+</span>
 
-    ></IconButton>
-<Icon type="info-tooltip" color="transparent" />
+
+<style>
+    .qc-tooltip {
+        display: inline-flex;
+        align-items: baseline;
+    }
+    .qc-tooltip-text {
+        border-bottom: 1px dashed var(--qc-color-text-primary);
+    }
+    .qc-tooltip-button {
+        align-self: center;
+        height: 16px;
+        width: 16px;
+        line-height: 16px;
+        margin-left: 4px;
+        display: block;
+        position: relative;
+    }
+</style>
