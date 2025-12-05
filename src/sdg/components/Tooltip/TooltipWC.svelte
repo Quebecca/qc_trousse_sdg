e2af856e53030a5f0577d94a7bfaa10a09856340
mmunos <marc.munos@mce.gouv.qc.ca>
2025-12-05 16:38:27 -0500

    wip - placement + focus

--- src/sdg/components/Tooltip/TooltipWC.svelte
+++ src/sdg/components/Tooltip/TooltipWC.svelte
@@ -3,12 +3,14 @@
   props: {
     text: { attribute: 'label', type: 'String' },
     description: { attribute: 'description', type: 'String' },
+    placement: { attribute: 'placement', type: 'String'}
   }
 }} />
 <script>
 
 import Tooltip from "./Tooltip.svelte";
 import {Utils} from "../utils";
+import {onMount} from "svelte";
 let { ...props } = $props();
 </script>
 <Tooltip {...props} ></Tooltip>
