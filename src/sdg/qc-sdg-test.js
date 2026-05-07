c00722a474eecac89befdd98b51bd201c392682d
mmunos <marc.munos@mce.gouv.qc.ca>
2026-05-07 09:29:13 -0400

    build: configuration Git LFS pour les snapshots PNG
    - Ajout .gitattributes pour tracker tests/snapshots/**/*.png via LFS
    - Les snapshots seront stockés en LFS pour alléger le dépôt

--- src/sdg/qc-sdg-test.js
+++ src/sdg/qc-sdg-test.js
@@ -2,10 +2,10 @@ import {setMcePivHeaderLogoSrc} from "./components/PivHeader/Test/pivHeaderTest"
 
 setMcePivHeaderLogoSrc();
 
-export * from './bases/Icon/Test/IconEmbeddedTest.svelte';
-export * from './components/TextField/Test/TextFieldEmbededTest.svelte';
+export * from './bases/Icon/Test/IconSvelteTest.svelte';
+export * from './components/TextField/Test/TextFieldSvelteTest.svelte';
 export * from "./components/DropdownList/Test/DropdownListSvelteTest.svelte";
-export * from "./components/ChoiceGroup/Test/ChoiceGroupeEmbededTest.svelte";
-export * from "./components/ToggleSwitch/Test/ToggleSwitchEmbeddedTest.svelte";
-export * from "./components/PivHeader/Test/pivHeaderEmbeddedTest.svelte";
+export * from "./components/ChoiceGroup/Test/ChoiceGroupSvelteTest.svelte";
+export * from "./components/ToggleSwitch/Test/ToggleSwitchSvelteTest.svelte";
+export * from "./components/PivHeader/Test/PivHeaderSvelteTest.svelte";
 export * from "./components/Alert/Test/AlertSvelteTest.svelte";
